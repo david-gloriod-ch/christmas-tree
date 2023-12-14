48b49fa701ae89cc13e8e5c95398cce6d6e49e70
David Gloriod <contact@david-gloriod.ch>
2023-12-14 03:59:16 +0100

    Adding editing timing system

--- sapin.js
+++ sapin.js
@@ -1,5 +1,7 @@
 // Get argument from command line
 const size = parseInt(process.argv[2]);
+const treeTiming = 0;
+const trunkTiming = treeTiming;
 
 if (isNaN(size) || size < 1) {
     console.log("Error: invalid size");
@@ -12,14 +14,14 @@ function printTree(size, i = 0) {
     if (i === size) return printTrunk(size, size - 1);
     console.log(" ".repeat(size - (i + 1)) + "* "
     .repeat(i + 1).substring(0, (i + 1) * 2 - 1));
-    timeout(() => printTree(size, i + 1), 300);
+    timeout(() => printTree(size, i + 1), treeTiming);
 }
 
 function printTrunk(size, i) {
     if (size < 2) return;
     console.log(" ".repeat(size - 1) + "|");
     if (i > 1)
-        timeout(() => printTrunk(size, i - 1), 300);
+        timeout(() => printTrunk(size, i - 1), trunkTiming);
 }
 
 function timeout(callback, delay) {
